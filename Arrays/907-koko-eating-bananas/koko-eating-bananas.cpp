aabd67cc122d60c4e18ce626db01151cf0171bec
Arnav <arnavkulkarni127@gmail.com>
2026-08-18 10:36:31 +0530

    Time: 8 ms (54.34%) | Memory: 23.1 MB (21.68%) - LeetSync

--- Arrays/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ Arrays/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,10 +4,15 @@ public:
         int low, high, mid;
         low = 1;
         high = *max_element(piles.begin(), piles.end());
-
+    
         while (low <= high) {
             mid = low + (high - low) / 2;
-            if (canFinish(piles,mid ) <= h) {
+            long long hours = 0;
+            for (int pile : piles) {
+            hours += (pile + mid - 1) / mid;
+            
+        }
+            if (hours <= h) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -16,12 +21,5 @@ public:
         return low;
     }
 
-    long long canFinish(vector<int>& piles,  int k) {
-        long long hours = 0;
-        for (int pile : piles) {
-            hours += (pile + k - 1) / k;
-            
-        }
-        return hours;
-    }
+  
 };
