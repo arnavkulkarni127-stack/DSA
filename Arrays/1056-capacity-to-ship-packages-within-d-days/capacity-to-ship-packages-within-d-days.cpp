d202a363fd9acd108c1a099e5d9d33f78e3b352e
Arnav <arnavkulkarni127@gmail.com>
2026-08-21 15:13:07 +0530

    Time: 11 ms (64.90%) | Memory: 35 MB (88.74%) - LeetSync

--- Arrays/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ Arrays/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int l = *max_element(weights.begin(), weights.end());
-        int r = accumulate(weights.begin(), weights.end(), 0);
-        while(l < r){
-            int mid = l + (r - l)/2;
-            int curr_load = 0, d = 1;
-            for(auto w : weights){
-                if(curr_load + w <= mid){
-                    curr_load += w;
-                }else{
-                    curr_load = w;
-                    d++;
+        int low, high, mid;
+        low = *max_element(weights.begin(), weights.end());
+        high = accumulate(weights.begin(), weights.end(), 0);
+        while (low < high){
+                mid = low + (high - low) / 2;
+                
+                if (enoughWeight(weights, days, mid)) {
+                    high = mid;
+                } else {
+                    low = mid+1;
                 }
             }
-            if(d <= days){
-                r = mid;
-            }else{
-                l = mid + 1;
-            }
-        }
-
-        return l;
+        return low;
     }
+    bool enoughWeight(vector<int>& weights, int days, int mid) {
+        int sum = 0;
+        int count = 1;
+        for (int weight:weights) {
+            if (sum + weight <= mid) {
+                sum += weight;
+            } else {
+                count++;
+                sum = weight;
+            }
+        
+        }
+          return count <= days ? true : false;
+    }
+
 };
