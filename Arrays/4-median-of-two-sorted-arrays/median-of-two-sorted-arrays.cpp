3e6441c932099f588f0de1675b426cea0a1b14b2
Arnav <arnavkulkarni127@gmail.com>
2026-08-30 22:03:36 +0530

    Time: 4 ms (18.88%) | Memory: 95 MB (79.88%) - LeetSync

--- Arrays/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ Arrays/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        int i, j, target, count, current, prev;
-        i = 0;
-        j = 0;
-    int total = m+n;
-    target = total/2;
-    count = 0;
-    prev = 0;
-    current = 0;
-    while(count <= target){
-        prev = current;
-        if(i < m && (j >= n || nums1[i]<nums2[j])){
-            current = nums1[i];
-            i++;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        else{
-            current = nums2[j];
-    j++;
-        }
-        count++;
-    }
-    if(current ==  0) return 0.00;
+        int total = n1 + n2;
+        int totalLeft = (n1 + n2 + 1) / 2;
+        int low = 0;
+        int high = n1;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = totalLeft - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
 
-    if(total % 2== 1){
-        return current;
-    }
-    else{
-        return (current + prev)/2.00;
-    }
+            if (l1 <= r2 && l2 <= r1) {
+                if (total % 2 == 1) {
+                    return max(l1, l2);
+                } else {
+                    return (double)((max(l1, l2) + min(r1, r2)) / 2.0);
+                }
+            } else if (l1 > r2)
+                high = mid1 - 1;
+            else {
+                low = mid1 + 1;
+            }
+        }
+        return 0;
     }
 };
