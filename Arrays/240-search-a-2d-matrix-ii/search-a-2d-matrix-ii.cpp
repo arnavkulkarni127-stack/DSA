2675f41a149a8fd9e1704df3825fcd358d66fe89
Arnav <arnavkulkarni127@gmail.com>
2026-09-01 19:22:11 +0530

    Time: 39 ms (94.85%) | Memory: 18.7 MB (39.27%) - LeetSync

--- Arrays/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ Arrays/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -4,21 +4,18 @@
 
             int m, n;
             m = matrix.size();
-            if(m == 0) return false;
+           // if(m == 0) return false;
             n = matrix[0].size();
-            if(n == 0) return false;
+           // if(n == 0) return false;
             int i, j;
             i = 0;
             j = n - 1;
-            if (j < 0) return false;
+           // if (j < 0) return false;
             while (i<m && j>=0) {
-                int current = matrix[i][j];
-                if (current == target)
-                    return true;
-                else if (target < current)
-                    j--;
-                else
-                    i++;
+               
+               if(matrix[i][j] < target) i++;
+               else if(matrix[i][j] > target) j--;
+               else return true;
             }
             return false;
         }
