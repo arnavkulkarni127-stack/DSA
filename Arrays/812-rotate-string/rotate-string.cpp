b5d302b8f3befd3e7f0f8bad1404a8d39875b673
Arnav <arnavkulkarni127@gmail.com>
2026-09-07 13:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (56.07%) - LeetSync

--- Arrays/812-rotate-string/rotate-string.cpp
+++ Arrays/812-rotate-string/rotate-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-    if(s.length() != goal.length()) return false;
+     string doubled = s + s;
 
-    string doubled = s + s;
+    if(s.length() != goal.length() || doubled.find(goal) == string::npos) return false;
 
-    return doubled.find(goal) != string::npos;
+ 
+    return true;
 
     }
 };
